37e162920dd6275f642fe8ec347c9b3ff0933e4d
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2017-11-01 18:34:59 -0400

    WIP wiki CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  resources :wikis
+
   devise_for :users
-  get 'welcome/index'
 
-  get 'welcome/about'
-
-  root 'welcome#index'
+  get 'about' => 'welcome#about'
+  root to: 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
