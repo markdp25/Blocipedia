d827d73c650d66a722f271d90f98dad96be12b1c
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2017-11-01 18:34:59 -0400

    WIP wiki CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'random_data'
+
+User.create(email: 'test@user.com', password: 'password',
+  confirmed_at: Time.now
+)
+
+# Create Wikis
+50.times do
+  Wiki.create!(
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+wikis = Wikis.all
+
+puts "Seed finished"
+puts "#{Wiki.count} wikis created"
