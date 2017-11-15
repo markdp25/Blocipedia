4c556a03454954da19c4a76c79067f82fd5cb30c
Mark Dellaposta <markdellaposta@Marks-MacBook-Pro.local>
2017-11-15 12:19:51 -0500

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  has_many :wikis, dependent: :destroy
-  has_many :collaborators, dependent: :destroy
+         has_many :wikis, dependent: :destroy
+         has_many :collaborations, dependent: :destroy
+         has_many :wiki_collaborations, through: :collaborations, source: :wiki
 
   before_save { self.email = email.downcase }
   before_save {self.role ||= :standard }
 
   enum role: [:standard, :premium, :admin]
   after_initialize :set_standard, :if => :new_record?
+
+  extend FriendlyId
+  friendly_id :name, use: :slugged
 
   def set_standard
     self.role ||= :standard
